5ca408b5a752b34c4d7b9d7fa068e0308e7de3f2
MORITA Kazutaka <morita.kazutaka@gmail.com>
2020-06-17 05:11:04 +0900

    call __wasm_call_ctors to invoke TVMBackendRegisterSystemLibSymbol

--- rust/runtime/tests/test_wasi_lib/src/lib.rs
+++ rust/runtime/tests/test_wasi_lib/src/lib.rs
@@ -1,5 +1,6 @@
 extern "C" {
     static __tvm_module_ctx: i32;
+    fn __wasm_call_ctors();
 }
 
 #[no_mangle]
@@ -18,8 +19,24 @@ use tvm_runtime::{DLTensor, Module as _, SystemLibModule};
 
 #[no_mangle]
 pub extern "C" fn run() {
+    unsafe {
+        // This is necessary to invoke TVMBackendRegisterSystemLibSymbol
+        // API calls.
+        __wasm_call_ctors();
+    }
     // try static
     let mut a = Array::from_vec(vec![1f32, 2., 3., 4.]);
     let mut b = Array::from_vec(vec![1f32, 0., 1., 0.]);
     let mut c = Array::from_vec(vec![0f32; 4]);
+    let e = Array::from_vec(vec![2f32, 2., 4., 4.]);
+    let mut a_dl: DLTensor = (&mut a).into();
+    let mut b_dl: DLTensor = (&mut b).into();
+    let mut c_dl: DLTensor = (&mut c).into();
+
+    let syslib = SystemLibModule::default();
+    let add = syslib
+        .get_function("default_function")
+        .expect("main function not found");
+    call_packed!(add, &mut a_dl, &mut b_dl, &mut c_dl).unwrap();
+    assert!(c.all_close(&e, 1e-8f32));
 }
